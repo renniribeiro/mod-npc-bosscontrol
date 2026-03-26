a7f2f8babfda28f54e7613c6a1e2e5249cea4d81
Renni Ribeiro <renni.ribeiro@gmail.com>
2026-03-26 08:59:58 -0300

    editando e renomeando

--- src/nm_loader.cpp
+++ src/nm_loader.cpp
@@ -1,6 +1,11 @@
-void AddSC_npc_bosscontrol();
+// Declaração da função que registra o script do NPC
+// Essa função foi definida no arquivo npc_morph.cpp
+void AddSC_npc_morph();
 
-void Addmod_npc_bosscontrolScripts()
+// Função principal do módulo (loader)
+// O nome normalmente segue padrão: Addmod_<nome>Scripts
+void Addmod_npc_morphScripts()
 {
-    AddSC_npc_bosscontrol();
+    // Chama a função que registra o script no core
+    AddSC_npc_morph();
 }
