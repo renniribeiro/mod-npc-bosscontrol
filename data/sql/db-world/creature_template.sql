78740a382114c08c1b1ded437922f2b446f7d5ca
Renni Ribeiro <renni.ribeiro@gmail.com>
2026-03-26 15:33:59 -0300

    atualizados

--- data/sql/db-world/creature_template.sql
+++ data/sql/db-world/creature_template.sql
@@ -2,25 +2,27 @@
 -- BOSSES
 -- ########################################
 
-DELETE FROM creature_template WHERE entry IN (80001,80002,80003);
+-- apaga npc, se existir
+DELETE FROM creature_template WHERE entry IN (80001,80002,80003,90000);
 
 -- Boss 1
-INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, rank, unit_class, unit_flags, type, ScriptName)
-VALUES (80001, 'Monstrinho', 'Nixcraft Server', 60, 60, 16, 0, 3, 1, 0, 7, 'npc_arena_boss1');
+INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, scale, `rank`, unit_class, unit_flags, type, mingold, maxgold, MovementType, ScriptName)
+VALUES (80001, 'Monstrinho', 'Nixcraft Server', 60, 60, 16, 0, .5, 3, 1, 0, 7, 10000, 20000, 1, 'npc_arena_boss1');
 
 -- Boss 2
-INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, rank, unit_class, unit_flags, type, ScriptName)
-VALUES (80002, 'Monstro', 'Nixcraft Server', 70, 70, 16, 0, 3, 1, 0, 7, 'npc_arena_boss2');
+INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, scale, `rank`, unit_class, unit_flags, type, mingold, maxgold, MovementType, ScriptName)
+VALUES (80002, 'Monstro', 'Nixcraft Server', 70, 70, 16, 0, 1, 3, 1, 0, 7, 20000, 30000, 1, 'npc_arena_boss2');
 
 -- Boss 3
-INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, rank, unit_class, unit_flags, type, ScriptName)
-VALUES (80003, 'Monstrengo Boss', 'Nixcraft Server', 80, 80, 16, 0, 3, 1, 0, 7, 'npc_arena_boss3');
+INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, scale, `rank`, unit_class, unit_flags, type, mingold, maxgold, MovementType, ScriptName)
+VALUES (80003, 'Monstrengo Boss', 'Nixcraft Server', 120, 120, 16, 0, 2, 3, 1, 0, 7, 50000, 100000, 1, 'npc_arena_boss3');
 
 -- ########################################
 -- CONTROLLER
 -- ########################################
 
-DELETE FROM creature_template WHERE entry = 90000;
+INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, ScriptName)
+VALUES (90000, 'Jonildo', 'Nixcraft Server', 80, 80, 35, 1, 'npc_bosscontrol');
 
-INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, ScriptName)
-VALUES (90000, 'Boss Controller', 'Arena System', 80, 80, 35, 1, 'npc_bosscontrol');
+-- mostra o que foi criado
+SELECT * FROM creature_template WHERE entry IN (80001,80002,80003,90000);
--- data/sql/db-world/creature_template_model.sql
+++ data/sql/db-world/creature_template_model.sql
@@ -1,7 +1,7 @@
 DELETE FROM creature_template_model WHERE CreatureID IN (90000,80001,80002,80003);
 
 -- Controller
-INSERT INTO creature_template_model VALUES (90000, 0, 18471, 1.0, 1, 0);
+INSERT INTO creature_template_model VALUES (90000, 0, 18047, 1.0, 1, 0);
 
 -- Boss 1
 INSERT INTO creature_template_model VALUES (80001, 0, 20522, 1.0, 1, 0);
@@ -11,3 +11,6 @@ INSERT INTO creature_template_model VALUES (80002, 0, 17028, 1.2, 1, 0);
 
 -- Boss 3
 INSERT INTO creature_template_model VALUES (80003, 0, 10193, 1.5, 1, 0);
+
+-- mostra o que foi criado
+SELECT * FROM creature_template_model WHERE CreatureID IN (80001,80002,80003,90000); 
