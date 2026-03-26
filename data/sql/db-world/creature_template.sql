d8521c705dbe8ba5377b39dc188609bd2c37e824
Renni Ribeiro <renni.ribeiro@gmail.com>
2026-03-26 17:15:01 -0300

    atualizados

--- data/sql/db-world/creature_template.sql
+++ data/sql/db-world/creature_template.sql
@@ -6,16 +6,16 @@
 DELETE FROM creature_template WHERE entry IN (80001,80002,80003,90000);
 
 -- Boss 1
-INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, scale, `rank`, unit_class, unit_flags, type, mingold, maxgold, MovementType, ScriptName)
-VALUES (80001, 'Monstrinho', 'Nixcraft Server', 60, 60, 16, 0, .5, 3, 1, 0, 7, 10000, 20000, 1, 'npc_arena_boss1');
+INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, `rank`, BaseAttackTime, RangeAttackTime, unit_class, unit_flags, unit_flags2, type, lootid, skinloot, mingold, maxgold, MovementType, mechanic_immune_mask, flags_extra, ScriptName)
+VALUES (80001, 'Monstrinho', 'Nixcraft Server', 60, 60, 16, 0, 3, 2000, 2000, 1, 0, 2048, 7, 80001, 70162, 10000, 20000, 1, 42016080, 1073741824, 'npc_arena_boss1');
 
 -- Boss 2
-INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, scale, `rank`, unit_class, unit_flags, type, mingold, maxgold, MovementType, ScriptName)
-VALUES (80002, 'Monstro', 'Nixcraft Server', 70, 70, 16, 0, 1, 3, 1, 0, 7, 20000, 30000, 1, 'npc_arena_boss2');
+INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, `rank`, BaseAttackTime, RangeAttackTime, unit_class, unit_flags, unit_flags2, type, lootid, skinloot, mingold, maxgold, MovementType, mechanic_immune_mask, flags_extra, ScriptName)
+VALUES (80002, 'Monstro', 'Nixcraft Server', 70, 70, 16, 0, 3, 2000, 2000, 1, 0, 2048, 7, 80001, 70162, 20000, 30000, 1, 42016080, 1073741824, 'npc_arena_boss2');
 
 -- Boss 3
-INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, scale, `rank`, unit_class, unit_flags, type, mingold, maxgold, MovementType, ScriptName)
-VALUES (80003, 'Monstrengo Boss', 'Nixcraft Server', 120, 120, 16, 0, 2, 3, 1, 0, 7, 50000, 100000, 1, 'npc_arena_boss3');
+INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, `rank`, BaseAttackTime, RangeAttackTime, unit_class, unit_flags, unit_flags2, type, lootid, skinloot, mingold, maxgold, MovementType, mechanic_immune_mask, flags_extra, ScriptName)
+VALUES (80003, 'Monstrengo Boss', 'Nixcraft Server', 120, 120, 16, 0, 3, 2000, 2000, 1, 0, 2048, 7, 80001, 70162, 50000, 100000, 1, 42016080, 1073741824, 'npc_arena_boss3');
 
 -- ########################################
 -- CONTROLLER
@@ -24,5 +24,26 @@ VALUES (80003, 'Monstrengo Boss', 'Nixcraft Server', 120, 120, 16, 0, 2, 3, 1, 0
 INSERT INTO creature_template (entry, `name`, subname, minlevel, maxlevel, faction, npcflag, ScriptName)
 VALUES (90000, 'Jonildo', 'Nixcraft Server', 80, 80, 35, 1, 'npc_bosscontrol');
 
+-- ########################################
+-- Loot para o Boss 3
+-- ########################################
+
+-- apaga se existir
+DELETE FROM creature_loot_template WHERE Entry = 80003;
+-- cria loot
+INSERT INTO creature_loot_template (Entry, Item, Chance, QuestRequired, LootMode,GroupId, MinCount, MaxCount ) VALUES
+-- 💰 Gold extra (simulado via item ou já usa mingold/maxgold)
+-- 🎁 Item raro (drop garantido)
+(80003, 49426, 100, 0, 1, 0, 1, 1),  -- Emblem of Frost
+
+-- 🟣 Equipamento (chance média)
+(80003, 50709, 40, 0, 1, 1, 1, 1),
+(80003, 50710, 40, 0, 1, 1, 1, 1),
+-- 🔵 Itens secundários
+(80003, 33470, 60, 0, 1, 0, 2, 5),  -- Frostweave Cloth
+-- 💎 Drop raro
+(80003, 36931, 15, 0, 1, 0, 1, 2);  -- Ametrine
+
 -- mostra o que foi criado
+SELECT * FROM creature_loot_template WHERE Entry = 80003;
 SELECT * FROM creature_template WHERE entry IN (80001,80002,80003,90000);
--- data/sql/db-world/creature_template_model.sql
+++ data/sql/db-world/creature_template_model.sql
@@ -4,13 +4,13 @@ DELETE FROM creature_template_model WHERE CreatureID IN (90000,80001,80002,80003
 INSERT INTO creature_template_model VALUES (90000, 0, 18047, 1.0, 1, 0);
 
 -- Boss 1
-INSERT INTO creature_template_model VALUES (80001, 0, 20522, 1.0, 1, 0);
+INSERT INTO creature_template_model VALUES (80001, 0, 20522, 0.3, 1, 0);
 
 -- Boss 2
-INSERT INTO creature_template_model VALUES (80002, 0, 17028, 1.2, 1, 0);
+INSERT INTO creature_template_model VALUES (80002, 0, 17028, 2.5, 1, 0);
 
 -- Boss 3
-INSERT INTO creature_template_model VALUES (80003, 0, 10193, 1.5, 1, 0);
+INSERT INTO creature_template_model VALUES (80003, 0, 10193, 2, 1, 0);
 
 -- mostra o que foi criado
 SELECT * FROM creature_template_model WHERE CreatureID IN (80001,80002,80003,90000); 
